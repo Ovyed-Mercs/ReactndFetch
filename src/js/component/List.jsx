49e5b9bbedffbe12e968fec05237105e03415042
OVYED Deno <nevad34@gmail.com>
2023-03-10 19:57:08 +0000

    update

---
+++ src/js/component/List.jsx
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from "react";
+
+//create your first component
+export const List = (props) => {
+	
+	return (
+		<ul className="list-group">
+  {props.toDos.map((item,index)=>{
+    return(<li className="list-group-item">{item.label}</li>)
+  })}
+</ul>
+	);
+};
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,21 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React,{useState,useEffect} from "react";
+import { List } from "./List.jsx";
 
 //create your first component
 const Home = () => {
+	const [toDos,setToDos]=useState([])
+	const [inputValue,setInputValue]=useState("")
+	let apiUrl='https://assets.breatheco.de/apis/fake/todos/user/OVYEDDeno'
+	const getList=()=> {
+		fetch(apiUrl).then(resp=>resp.json()).then(data=>setToDos(data)).catch(error=>console.log(error))
+	}
+	useEffect(() => {
+	getList()
+	  }, []);
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<List toDos={toDos}/>
 		</div>
 	);
 };
